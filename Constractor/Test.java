11b60ccafc76dad8ddbe24dfb0a2d1c0b56a351f
Nur Jannat <104066871+terminalinfinity@users.noreply.github.com>
2023-01-03 06:25:10 -0800

    Add files via upload

---
+++ Constractor/Test.java
@@ -0,0 +1,11 @@
+package Constractor;
+
+public class Test {
+    public static void main(String[] args) {
+        Student s1=new Student("Nur","Female",20,10232);
+        Student s2=new Student("Jannat","Female",22,10233);
+
+        s1.display();
+        s2.display();
+    }
+}
