e1efcc9bca084b2f5d10c17b626174e5d0ba7204
Nur Jannat <104066871+terminalinfinity@users.noreply.github.com>
2023-02-26 08:00:08 -0800

    Add files via upload

---
+++ StaticMathod/Method.java
@@ -0,0 +1,11 @@
+package StaticMathod;
+
+public class Method {
+    static int x=5;
+    void display1(){
+        System.out.println("Value of x in Display1() is:"+x);
+    }
+    static void display2(){
+        System.out.println("Value of x in Display2() is:"+x);
+    }
+}
---
+++ StaticMathod/Test.java
@@ -0,0 +1,9 @@
+package StaticMathod;
+
+public class Test {
+    public static void main(String[] args) {
+        Method obj=new Method();
+        obj.display1();
+        Method.display2();
+    }
+}
